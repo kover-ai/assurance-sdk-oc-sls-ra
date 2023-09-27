6af66ea8ba9cc86e8a05d1f36ecd1490dbe58b5b
winter <winter.ld@outlook.com>
2023-09-27 14:33:11 +0800

    feat: update http request header (x-key, x-version)

--- SeelKit/SeelKit/SKHttp/SKRequest.m
+++ SeelKit/SeelKit/SKHttp/SKRequest.m
@@ -212,8 +212,11 @@
 - (void)onConfigRequestHeSKer:(SKHTTPSessionManager *)manager
 {
     [manager.requestSerializer setCachePolicy:NSURLRequestReloadIgnoringLocalCacheData];
-    NSString *key = [SKAuthManager shared].key;
-    [manager.requestSerializer setValue:key     forHTTPHeaderField:@"API-key"];
+    // NSString *key = [SKAuthManager shared].key;
+    NSString *key = @"e2b4f9c8d3a7e6d2f5b0c4a1d8e9b6";
+    NSString *version = @"1.3.0";
+    [manager.requestSerializer setValue:key     forHTTPHeaderField:@"X-Seel-API-Key"];
+    [manager.requestSerializer setValue:version  forHTTPHeaderField:@"X-Seel-API-Version"];
 }
 
 #pragma mark ---配置请求参数
